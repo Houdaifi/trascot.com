246e73986b7a8483438e2572ba7a46079b555c70
Houdaifa Amzil <35547273+Houdaifi@users.noreply.github.com>
2021-09-15 15:25:42 +0000

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-gray-50">
+    <navbar/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Navbar
+  }
+}
+</script>
